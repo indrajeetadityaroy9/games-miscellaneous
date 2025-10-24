7175a24992827f80c1b9b2840fdbd0fc5aaf8fc8
Indrajeet Aditya Roy <roy.indrajeet009@gmail.com>
2025-10-23 21:26:46 -0400

    Configure landing page for GitHub Pages

--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace 'games-misc' with your repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/games-misc/',
 })
